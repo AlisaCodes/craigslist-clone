5568c503f281976d6562d1ce32e171f52c19225d
alyssa horrocks & alisa colon <student@epicodus.com>
2015-09-29 10:40:05 -0700

    Allow user to delete a listing.

--- app/routes/listing.js
+++ app/routes/listing.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('listing', params.listing_id);
   },
+
+  actions: {
+    delete(model) {
+      model.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
